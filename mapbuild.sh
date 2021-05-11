53e5d39a42335d7a3bb106921bbcb7851423f81e
magnolia61 <richard.van.oosterhout@gmail.com>
2021-05-11 18:15:52 +0200

    comment mapbuild

--- mapbuild.sh
+++ mapbuild.sh
@@ -1,3 +1,3 @@
 npm run build
-rsync -asvz --delete /var/www/vhosts/idqprod/dist/ /var/www/vhosts/idqprod/web/
+#rsync -asvz --delete /var/www/vhosts/idqprod/dist/ /var/www/vhosts/idqprod/web/
 sudo chown -R webteam:www-data /var/www/vhosts/idqprod/web/
