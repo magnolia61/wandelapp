47f8ddeada8390c3285d746802ae0c2efb023806
IDC Webteam <webteam@h2915379.stratoserver.net>
2021-08-13 06:11:44 +0200

    mapbuild

--- mapbuild.sh
+++ mapbuild.sh
@@ -1,3 +1,3 @@
 npm run build
-rsync -asvz --delete /var/www/vhosts/idqprod/dist/ /var/www/vhosts/idqprod/web/nwg/plof/
+rsync -asvz --delete /var/www/vhosts/idqprod/dist/ /var/www/vhosts/idqprod/web/ede/znds/
 sudo chown -R webteam:www-data /var/www/vhosts/idqprod/web/
